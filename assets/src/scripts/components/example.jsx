6164d8de27c16b349e125f98c72265fcb4cc3f74
Rafael Bitencourt <rafael@bitencourt.ca>
2015-07-30 22:05:34 -0400

    Clean main.js files and delete some example files

---
+++ assets/src/scripts/components/example.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Example extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return <p>Hello from React</p>;
+	}
+}
--- assets/src/scripts/components/hello-from-react.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Hello extends React.Component {
-	render() {
-		return <p>Hello from React</p>;
-	}
-}
-
-export default Hello;
